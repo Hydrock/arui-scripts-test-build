a64452ada434072c274df0a67f69830838aaa893
Вечканов Алексей Сергеевич <avechkanov@alfabank.ru>
2021-10-28 10:42:43 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app';
-import { AppContainer } from 'react-hot-loader';
 
 ReactDOM.render(
-    <AppContainer><App /></AppContainer>,
+    <App />,
     document.getElementById('react-app')
 );
-if (module.hot) {
-    module.hot.accept('./app', () => {
-        const NextAppAssignments = require('./app').default;
-        ReactDOM.render(
-            <AppContainer><NextAppAssignments /></AppContainer>,
-            document.getElementById('react-app')
-        );
-    });
-}
+// if (module.hot) {
+//     module.hot.accept('./app', () => {
+//         const NextAppAssignments = require('./app').default;
+//         ReactDOM.render(
+//             <NextAppAssignments />,
+//             document.getElementById('react-app')
+//         );
+//     });
+// }
